f0446da86a8fdc05473bc41c49dc3aa04d9e971f
Juan Carlos <jcarczapata@gmail.com>
2022-03-13 15:04:05 -0500

    Fix PMD issues

--- hotel/src/main/java/com/jccz/management/hotel/dto/UserDto.java
+++ hotel/src/main/java/com/jccz/management/hotel/dto/UserDto.java
@@ -1,7 +1,6 @@
 package com.jccz.management.hotel.dto;
 
 import lombok.Data;
-import lombok.experimental.Accessors;
 
 @Data
 public class UserDto {
--- hotel/src/main/java/com/jccz/management/hotel/entities/User.java
+++ hotel/src/main/java/com/jccz/management/hotel/entities/User.java
@@ -1,12 +1,7 @@
 package com.jccz.management.hotel.entities;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.Accessors;
-
 import javax.persistence.*;
-import java.util.UUID;
 
 @Entity
 @Table(name = "user")
